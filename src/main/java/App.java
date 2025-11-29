a0d7a49e1617a5234cd4d9ab64a687353dca5e64
yinkaowolabi091-web <yinkaowolabi091@gmail.com>
2025-11-29 17:21:02 +0100

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins CI/CD!");
+        System.out.println("Hello, Jenkinsss CI/CD!");
     }
 }
